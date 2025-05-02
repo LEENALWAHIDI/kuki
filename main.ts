d73485b28e1224e5c027c050feab28b901393a8a
LEENALWAHIDI <leenal@post.bgu.ac.il>
2025-05-02 18:18:58 +0300

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
+    led.toggle(0, 0)
 	
+    loops.everyInterval(500, function() {
+        
+    })
 })
+music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
